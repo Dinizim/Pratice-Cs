2904eebb388bc3133e36b60bbc0dc2b1376b81b7
Nicollas Diniz <nicollasfdiniz@gmail.com>
2024-01-13 19:52:19 -0300

    add comands

--- To-do List/To-do List/Program.cs
+++ To-do List/To-do List/Program.cs
@@ -13,32 +13,19 @@ namespace To_do_List
         static void Main(string[] args)
         {
 
+            
 
-
-            List<Class_Task> List_Task = new List<Class_Task>();
-            List_Task = Class_Task.View_List_Task(@"C:\Users\nicol\OneDrive\Documentos\GitHub\Pratice-C#\To-do List\task2.json");
-            Class_Task Task = new Class_Task();
-            Task.Task_Name = "limpar cozinha a noite";
-            Task.Task_Cheked = false;
-            List_Task.Add(Task);
-            Class_Task Task2 = new Class_Task();
-            Task2.Task_Name = "limpar cozinha DNVVVVVV";
-            Task2.Task_Cheked = false;
-            List_Task.Add(Task2);
-
-
-            /*
-            var tasks = new Class_Task();
-            if (tasks.Add_List_Task(List_Task, @"C:\Users\nicol\OneDrive\Documentos\GitHub\Pratice-C#\To-do List\task2.json"))
-                Console.WriteLine("salvo");
-            */
-            List<string> Commands = new List<string> {"Add + Task", "Check + Task", "Del + Task" };
+            List<string> Commands = new List<string> {"ADD + Task", "CHECK + Task", "DEL + Task","Exit = crtl + n" };
 
             int exit = 1;
+            List<Class_Task> List_Task = new List<Class_Task>();
+            List_Task = Class_Task.View_List_Task(@"C:\Users\nicol\OneDrive\Documentos\GitHub\Pratice-C#\To-do List\task2.json");
             do
             {
+                
                 Console.Clear();
                 string NewTask = " ";
+                Console.WriteLine("Press Ctrl + D to see the commands...");
                 Console.WriteLine();
                 Console.WriteLine(" TO-DO LIST");
                 Console.WriteLine("==================");
@@ -47,6 +34,7 @@ namespace To_do_List
 
                 foreach (var task in List_Task)
                 {
+                    task.Task_Name = task.Task_Name.ToUpper();
                     if (task.Task_Cheked)
                     {
                         Console.ForegroundColor = ConsoleColor.Green;
@@ -60,30 +48,93 @@ namespace To_do_List
 
                     Console.ResetColor();
                     Console.WriteLine(task.Task_Name);
+
                 }
                 Console.WriteLine();
-                Console.WriteLine("Press the Commands :");
-                Console.WriteLine("Press Ctrl + D to see the commands...");
-                NewTask = Console.ReadLine();
+                Console.WriteLine("Press the Command :");
+               
 
-                if(NewTask.ToLower() == "\u0004")
+
+                    NewTask = Console.ReadLine();
+                if (NewTask != "\u0004" && NewTask != "\u000e")
                 {
-                    Console.Clear();
-                    Console.WriteLine("Comandos disponíveis:");
-                    foreach (var command in Commands)
+                    
+                    if (NewTask.Substring(0, 3).Equals("DEL"))
                     {
-                        Console.WriteLine(command);
+                        string task = NewTask.Substring(4);
+                        for (int i = 0; i < List_Task.Count; i++)
+                        {
+                            string Task_Name_WithoutSpaces = List_Task[i].Task_Name.Replace(" ", "");
+                            if (Task_Name_WithoutSpaces.Equals(task))
+                            {
+                                List_Task.RemoveAt(i);
+                            }
+                        }
                     }
-                    Console.ReadLine();
+                    if (NewTask.Substring(0, 5).Equals("CHECK"))
+                    {
+                        string task = NewTask.Substring(6);     
 
+                        for (int i = 0; i < List_Task.Count; i++)
+                        {
+                            string Task_Name_WithoutSpaces = List_Task[i].Task_Name.Replace(" ", "");
+                            if (Task_Name_WithoutSpaces.Equals(task))
+                            {
+                                List_Task[i].Task_Cheked = true;
+                            }
+                        }
+                    }
+
+                    if (NewTask.Substring(0, 4).Equals("ADD "))
+                    {
+
+
+                        Class_Task Task = new Class_Task();
+                        string Addtask = NewTask.Substring(4);
+                        Task.Task_Name = Addtask;
+                        Task.Task_Cheked = false;
+                        List_Task.Add(Task);
+                    }
                 }
+                switch (NewTask.ToLower())
+                    {
+
+
+                        case "\u000e":
+                            exit = 0;
+                            break;
+
+
+                        case "\u0004":
+
+                            Console.Clear();
+                            Console.WriteLine("Comandos disponíveis:");
+                            foreach (var command in Commands)
+                            {
+                                Console.WriteLine(command);
+                            }
+                            Console.ReadLine();
+
+                            break;
+                    }
+                
+
 
             } while(exit != 0);
+
+
+           
+            var tasks = new Class_Task();
+
+            if (tasks.Add_List_Task(List_Task, @"C:\Users\nicol\OneDrive\Documentos\GitHub\Pratice-C#\To-do List\task2.json"))
+                Console.WriteLine("Todo list Salva!");
+            Console.ReadLine();
+           
         }
 
-        
 
-        
+
+
 
     }
 }
